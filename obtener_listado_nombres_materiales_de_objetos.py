cb05b897cd0fa80dcaf692466f595f51c5ba0dd5
zebus3d <zebus3dream@gmail.com>
2012-12-20 19:02:56 +0100

    Update obtener_listado_nombres_materiales_de_objetos.py
    conseguir listado de nombre de materiales de objetos en local y en todas las escenas con selección o sin selección.

--- obtener_listado_nombres_materiales_de_objetos.py
+++ obtener_listado_nombres_materiales_de_objetos.py
@@ -1,22 +1,35 @@
-import bpy
-
 # optener listado de materiales de objetos
 def obtenermateriales(opcion1):
     mat_list = []
     if opcion1 == "local":
-        tipo = bpy.context.selected_objects
+        sel = bpy.context.selected_objects
+        if len(sel) == 0:    
+            for os in bpy.context.scene.objects:
+                for slot in os.material_slots:
+                    mat_list.append(slot)
+        else:
+            for os in sel:
+                for slot in os.material_slots:
+                    mat_list.append(slot)
     else:
-        tipo = bpy.data.objects    
-    if len(tipo) == 0:
-        for m in bpy.context.scene.objects:
-            for slot in m.material_slots:
-                mat_list.append(slot)
-    else:
-        for m in tipo:
-            for slot in m.material_slots:
-                mat_list.append(slot)
+        sel = bpy.context.selected_objects
+        for escena in bpy.data.scenes:
+            for os in bpy.data.scenes[escena.name].objects:
+                if len(sel) == 0:
+                    for slot in os.material_slots:
+                        mat_list.append(slot)
+                else:
+                    for so in sel:
+                        for slot in so.material_slots:
+                            mat_list.append(slot)   
+    # limpiando lista de repetidos:
+    preparando = list(set(mat_list))
+    mat_list = preparando
     return mat_list
-    
+
+# Nota: si no es en "local" y si es con algo seleccionado, evidentemente 
+# saldran solo los de los objetos deleccionados de la escena actual, ya 
+# que los full copy scene cambian de nombre los objetos y son "unicos"
 mat = obtenermateriales("local")
 for i in mat:
     print(i.name)
