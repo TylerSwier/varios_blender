535a4d1b4bb84a0224b46384910f894857e002b7
zebus3d <zebus3dream@gmail.com>
2014-05-07 16:49:41 +0200

    Create insert_keyframe_in_all_objects_in_all_frames.py

---
+++ insert_keyframe_in_all_objects_in_all_frames.py
@@ -0,0 +1,15 @@
+import bpy
+
+scn = bpy.context.scene
+inicio = bpy.data.scenes[scn.name].frame_start
+final = bpy.data.scenes[scn.name].frame_end
+
+def accion_insert_keyframe(f):
+    for o in bpy.data.objects:
+        scn.frame_set(f) #<- indicamos el frame
+        o.keyframe_insert(data_path="scale", index=-1) #<- insertamos el keyframe
+        
+for f in range(inicio, final):
+    accion_insert_keyframe(f)
+
+bpy.ops.screen.frame_jump() #<- vamos hasta el frame 1
