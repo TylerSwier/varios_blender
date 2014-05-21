7285032432f79a790ce84e9513215542e3f15b06
zebus3d <zebus3dream@gmail.com>
2014-05-21 07:54:51 +0200

    Update matte_multilayer_v00.py
    agregando el material shadeless a los objetos...

--- matte_multilayer_v00.py
+++ matte_multilayer_v00.py
@@ -4,6 +4,14 @@
 import bpy
 objetos = bpy.context.selected_objects
 scn = bpy.context.scene
+# creando el material:
+matName = 'shadeless_mt'
+bpy.data.materials.new(matName)
+bpy.data.materials[matName].use_nodes = True
+bpy.data.materials[matName].node_tree.nodes.new(type='ShaderNodeEmission')
+inp = bpy.data.materials[matName].node_tree.nodes['Material Output'].inputs['Surface']
+outp = bpy.data.materials[matName].node_tree.nodes['Emission'].outputs[0]
+bpy.data.materials[matName].node_tree.links.new(inp,outp)
 # en el area de view 3d:
 for window in bpy.context.window_manager.windows:
     screen = window.screen
@@ -14,10 +22,19 @@ for window in bpy.context.window_manager.windows:
             for i in range(len(objetos)):
                 bpy.ops.object.select_all(action='DESELECT')
                 scn.objects.active = objetos[i]
-                objetos[i].select = True    
+                objetos[i].select = True
+                # desactivo un par de cosas para q los shaders emission no emitan luz y solo sean shadeless:
+                objetos[i].cycles_visibility.diffuse = False
+                objetos[i].cycles_visibility.transmission = False
+                # poniendo cada objeto seleccionado en un layer:
                 posicion[i] = True # indico el layer
                 bpy.ops.object.move_to_layer(layers=(posicion)) # lo seteo
-                posicion[i] = False # dejo el layer como estaba
+                posicion[i] = False # dejo el layer como estaba                
+                # aplicandole el material:
+                #ultimomat = bpy.data.materials[len(bpy.data.materials)-1]
+                #bpy.context.selected_objects[0].material_slots[0].material = ultimomat
+                objetos[i].active_material = bpy.data.materials[matName]
+                #objetos[i].material_slots[0].material =bpy.data.materials[matName]                
             # por cada objeto creo un render layer
             cuantos = len(objetos)
             for i in range(cuantos-1):
