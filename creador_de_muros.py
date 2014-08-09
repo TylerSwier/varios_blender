7364db40908e55a057baec088e84195d1e330ee2
zebus3d <zebus3dream@gmail.com>
2014-08-09 08:17:48 +0200

    Update creador_de_muros.py
    update

--- creador_de_muros.py
+++ creador_de_muros.py
@@ -5,34 +5,39 @@ bpy.context.scene.unit_settings.system = 'METRIC'
 
 alto = 10
 ancho = 8
-def fabricarLadrillosHorizontal():
-    # offset de nacimiento
-    n_offset=0
-    # individual offsets:
-    h_offset=0.40
-    v_offset=0.115
-    for v in range(alto):
-        y=v
-        for h in range(ancho):
-            x=h
-            bpy.ops.mesh.primitive_cube_add(radius=1, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-            bpy.context.object.name = "ladrillo"
-            ob = bpy.context.object
-            # Ladrillo estandar en centimetros:            
-            # poniendo la escala correspondiente a los centimetro...
-            # ob.dimensions.x = sx
-            # 40cm 6cm 11.5cm:
-            # son las cifras de arriba pero con: /2
-            ob.scale.x = 0.2
-            ob.scale.y = 0.03
-            ob.scale.z = 0.0575
-            # lo pongo en su sitio con el suelo Z:
-            ob.location.z = 0.0575
-            if y%2 == 0:
-                ob.location.x += x*h_offset+n_offset+0.20
-                ob.location.z = y*v_offset+n_offset
-            else:     
-                ob.location.x = x*h_offset+n_offset 
-                ob.location.z = y*v_offset+n_offset
-            
-fabricarLadrillosHorizontal()
+
+# Ladrillo estandar en centimetros:            
+# 40cm 6cm 11.5cm:
+lancho=40
+lato=6
+llargo=11.5
+# conversion a cm:
+cm_lancho=lancho/2
+cm_lalto=lato/2
+cm_llargo=llargo/2
+
+# offset de nacimiento vertical
+# lo pongo en su sitio con el suelo Z:
+nv_offset = cm_llargo
+nh_offset = 0
+# individual offsets (donde iria el cemento):
+h_offset=lancho
+v_offset=llargo
+
+for v in range(alto):
+    y=v
+    for h in range(ancho):
+        x=h
+        bpy.ops.mesh.primitive_cube_add(radius=1, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+        bpy.context.object.name = "ladrillo"
+        ob = bpy.context.object
+        ob.scale.x = cm_lancho
+        ob.scale.y = cm_lalto
+        ob.scale.z = cm_llargo
+        
+        if y%2 == 0:
+            ob.location.x += x*h_offset+nh_offset+cm_lancho
+            ob.location.z = y*v_offset+nv_offset
+        else:     
+            ob.location.x = x*h_offset+nh_offset 
+            ob.location.z = y*v_offset+nv_offset
