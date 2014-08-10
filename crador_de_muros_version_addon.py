bdca4250d7c141c85e2ba63c8bb94e9b9f7e96ec
zebus3d <zebus3dream@gmail.com>
2014-08-10 19:34:59 +0200

    Update crador_de_muros_version_addon.py
    arreglado jeje falta implementar alguna cosa mas...

--- crador_de_muros_version_addon.py
+++ crador_de_muros_version_addon.py
@@ -27,6 +27,22 @@ bl_info = {
     "tracker_url": "",
     "category": "Add Mesh"}
 
+def mover(nombre, x, y):
+    ob = bpy.data.objects[str(nombre)]
+    # en metros un cubo de escala 1 1 1 equivale a 2 metros x 2 metros x 2 metros..
+    # por lo tanto :
+    x = x*2
+    y = y*2
+    ob.location.x = x
+    ob.location.z = y
+
+def selecccionarPorNombre(nombre):
+    ob = bpy.data.objects[str(nombre)]
+    ob.select = True
+
+def desseleccionarTodo():
+    bpy.ops.object.select_all(action='DESELECT')
+    
 def eliminarTodo():
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
@@ -35,136 +51,64 @@ def freezer():
     bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
 
-def creandoLadrillo(nombre):
+def creandoLadrillo(nombre, sizex, sizey, sizez):
     bpy.ops.mesh.primitive_cube_add(radius=1, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-    bpy.context.object.name = nombre
+    ob = bpy.context.object
+    ob.name = nombre
+    ob.scale.x = sizex
+    ob.scale.y = sizey
+    ob.scale.z = sizez
 
 class DialogOperator(bpy.types.Operator):
     bl_idname = "object.zebus_dialog"
     bl_label = "Wall Bricks Creator v01"
-    my_alto = IntProperty(name="alto", min=0, max=100, default=20)
-    my_ancho = IntProperty(name="ancho", min=0, max=100, default=5)
-    my_m_o_c = BoolProperty(name="Centimeters", default=False)
-    #my_fillb = BoolProperty(name="Fill Boundaryes", default=True)
-    #my_lancho = FloatProperty(name="lancho", min=0, max=100, default=24)
-    #my_lalto = FloatProperty(name="lalto", min=0, max=100, default=11.2)
-    #my_llargo = FloatProperty(name="llargo", min=0, max=100, default=5)
-    #my_cemento = FloatProperty(name="cemento", min=0, max=1.8, default=1.6)
     
-    my_fillb = BoolProperty(name="Fill Boundaryes", default=False)
-    my_lancho = FloatProperty(name="lancho", min=0, max=100, default=0.5)
-    my_lalto = FloatProperty(name="lalto", min=0, max=100, default=0.5)
-    my_llargo = FloatProperty(name="llargo", min=0, max=100, default=0.5)
-    my_cemento = FloatProperty(name="cemento", min=0, max=1.8, default=0.5)
+    # Defaults:
+    muro_alto = IntProperty(name="alto", min=0, max=100, default=4)
+    muro_ancho = IntProperty(name="ancho", min=0, max=100, default=4)
+       
+    ladrillo_ancho = FloatProperty(name="ladrillo_ancho", min=0, max=100, default=1)
+    ladrillo_alto = FloatProperty(name="ladrillo_alto", min=0, max=100, default=1)
+    ladrillo_largo = FloatProperty(name="ladrillo_largo", min=0, max=100, default=1)
+    cemento = FloatProperty(name="cemento", min=0, max=1.8, default=0)
     
-    # en metros un cubo de escala 1 1 1 equivale a 2 metros x 2 metros x 2 metros...
     
     def execute(self,context):
         
         eliminarTodo()
-        
-        alto = self.my_alto
-        ancho = self.my_ancho
-        ladrillo_ancho = self.my_lancho
-        ladrillo_alto = self.my_lalto
-        ladrillo_largo = self.my_llargo
-        en_cm = self.my_m_o_c
-        fill_esquinas = self.my_fillb
-        cemento = self.my_cemento
-       
-       
+        # pongo blender en metros:
         bpy.context.scene.unit_settings.system = 'METRIC'
-        ladrillo_ancho_cm=self.my_lancho
-        ladrillo_alto_cm=self.my_lalto
-        ladrillo_largo_cm=self.my_llargo
-            
-        #if self.my_m_o_c:
-            ## conversion a cm:
-            ## no se por que me los esta multiplicando por 2 asi que /2
-            #bpy.context.scene.unit_settings.system = 'METRIC'
-            #ladrillo_ancho_cm=self.my_lancho/2/100
-            #ladrillo_alto_cm=self.my_lalto/2/100
-            #ladrillo_largo_cm=self.my_llargo/2/100
-        #else:
-            #bpy.context.scene.unit_settings.system = 'METRIC'
-            #ladrillo_ancho_cm=self.my_lancho/2
-            #ladrillo_alto_cm=self.my_lalto/2
-            #ladrillo_largo_cm=self.my_llargo/2
-            
-        # offset de nacimiento vertical
-        # lo pongo en su sitio con el suelo Z:
-        
-        
-        # individual offsets (donde iria el cemento):
-        c_offset=cemento
-        #if cemento != 0:
-            #c_offset=cemento
-
-        #else:
-            #c_offset=ladrillo_ancho_cm
-           
-        
-        for v in range(alto):
-            y=v
-            for h in range(ancho):
-                x=h
-                creandoLadrillo("ladrillo")
-                ob = bpy.context.object
-                ob.scale.x = ladrillo_ancho_cm
-                ob.scale.y = ladrillo_alto_cm
-                ob.scale.z = ladrillo_largo_cm
                 
-                # si son pares:
-                if y%2 == 0:
-                    anterior=ob.name
-                    
-                    # Boundaryes:
-                    #if fill_esquinas:
-                        #if h == 0:
-                            #bpy.ops.object.select_all(action='DESELECT')
-                            #creandoLadrillo("ladrillo_bordes")
-                            #actual = bpy.context.selected_objects[0].name
-                            
-                            #ob = bpy.data.objects[str(actual)]
-                            #ob.select = True            
-                            #ob = bpy.context.object
-                            #ob.scale.x = ladrillo_ancho_cm/2
-                            #ob.scale.y = ladrillo_alto_cm
-                            #ob.scale.z = ladrillo_largo_cm
-                            #ob.location.x = x * c_offset + nh_offset + ladrillo_ancho_cm
-                            #ob.location.z = y * c_offset + nv_offset
-                            
-                            #freezer()
-                            
-                        #if (h == ancho-1):
-                            #bpy.ops.object.select_all(action='DESELECT')
-                            #creandoLadrillo("ladrillo_bordes")
-                            #actual = bpy.context.selected_objects[0].name
-
-                            #ob = bpy.data.objects[str(actual)]
-                            #ob.select = True            
-                            #ob = bpy.context.object
-                            #ob.scale.x = ladrillo_ancho_cm/2
-                            #ob.scale.y = ladrillo_alto_cm
-                            #ob.scale.z = ladrillo_largo_cm
-                            #ob.location.x = x * c_offset + nh_offset + ladrillo_ancho_cm
-                            #ob.location.z = y * c_offset + nv_offset + c_offset
-                            
-                            #freezer()
-                            
-                    bpy.ops.object.select_all(action='DESELECT')
-                    ob = bpy.data.objects[str(anterior)]
-                    ob.select = True            
-                    ob.location.x = (x * (c_offset * 2)) + (ladrillo_ancho_cm * 2) + c_offset
-                    ob.location.z = y * ((ladrillo_alto_cm * 2) + c_offset)
-                    
+        muro_alto = self.muro_alto
+        muro_ancho = self.muro_ancho
+        
+        ladrillo_alto = self.ladrillo_alto
+        ladrillo_ancho = self.ladrillo_ancho
+        ladrillo_largo = self.ladrillo_largo
+        
+        cemento = self.cemento
+        
+        # array 2 dimensiones
+        for v in range(muro_alto):
+            for h in range(muro_ancho):
+                
+                creandoLadrillo("ladrillo", ladrillo_largo, ladrillo_ancho, ladrillo_alto)
+                ob = bpy.context.object
+                
+                ## si son pares:
+                if v%2 == 0:
+                    #anterior=ob.name
+                    #desseleccionarTodo()                    
+                    #selecccionarPorNombre(anterior)
+                    x = h * ladrillo_largo * (cemento+1) + (ladrillo_ancho/2)
+                    y = v * ladrillo_alto * (cemento+1)
+                    mover(ob.name, x, y)
                     freezer()
-                    
                 # si no son pares:
                 else:     
-                    ob.location.x = (x * (c_offset * 2)) + (ladrillo_ancho_cm * 2)
-                    ob.location.z = y * ((ladrillo_alto_cm * 2) + c_offset)
-                    
+                    x = h * ladrillo_largo * (cemento+1)
+                    y = v * ladrillo_alto * (cemento+1)
+                    mover(ob.name, x, y)
                     freezer()
             
                     
@@ -177,19 +121,23 @@ class DialogOperator(bpy.types.Operator):
         layout = self.layout
         box = layout.box()
         col = box.column()
+        
         col.label("Wall Settings:")
         rowsub0 = col.row()
-        rowsub0.prop(self, "my_alto", text='Alto')
-        rowsub0.prop(self, "my_ancho", text='Ancho')
+        rowsub0.prop(self, "muro_alto", text='M Alto')
+        rowsub0.prop(self, "muro_ancho", text='M Ancho')
+        
         col.label("Brick Settings:")
-        col.prop(self, "my_lancho", text='Alto')
-        col.prop(self, "my_lalto", text='Ancho')
-        col.prop(self, "my_llargo", text='Largo')
+        col.prop(self, "ladrillo_alto", text='Alto')
+        col.prop(self, "ladrillo_ancho", text='Ancho')
+        col.prop(self, "ladrillo_largo", text='Largo')
         col.label("Cement Settings:")
-        col.prop(self, "my_cemento", text='Cemento')
+        col.prop(self, "cemento", text='Cemento')
+        
         col.label("Units Settings:")
         col = box.column()
         col.prop(self, "my_m_o_c")
+        
         col.label("Fill Settings:")
         col.prop(self, "my_fillb")
             
