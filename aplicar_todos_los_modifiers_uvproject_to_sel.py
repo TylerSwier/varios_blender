688f45c2ecaf0e2e84657a6af36c38ff02aedfa0
zebus3d <zebus3dream@gmail.com>
2014-08-13 19:28:19 +0200

    Update aplicar_todos_los_modifiers_uvproject_to_sel.py
    update

--- aplicar_todos_los_modifiers_uvproject_to_sel.py
+++ aplicar_todos_los_modifiers_uvproject_to_sel.py
@@ -1,8 +1,8 @@
 import bpy
 objetos=bpy.context.selected_objects[:]
 for ob in objetos:
-    bpy.ops.object.select_all(action='DESELECT')
     ob = bpy.data.objects[ob.name]
     ob.select = True
+    bpy.context.scene.objects.active = ob
     if "UV_PROJECT" in ob.modifiers:
         bpy.ops.object.modifier_apply(apply_as='DATA', modifier="UV_PROJECT")
