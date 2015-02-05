89a62fda35cb1af2953fb0caf4e364d7c53a0987
zebus3d <zebus3dream@gmail.com>
2015-02-05 11:53:12 +0100

    Create creando_una_cuerda.py
    primera fase

---
+++ creando_una_cuerda.py
@@ -0,0 +1,32 @@
+import bpy
+bpy.ops.mesh.primitive_plane_add(radius=1, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+bpy.ops.object.editmode_toggle()
+bpy.ops.mesh.delete(type='VERT')
+
+bpy.ops.object.editmode_toggle()
+bpy.context.selected_objects[0].data.vertices.add(1)
+bpy.context.selected_objects[0].data.update
+bpy.ops.object.editmode_toggle()
+
+longitud=5
+cuantos_segmentos=25
+for i in range(cuantos_segmentos):
+    bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":(longitud/cuantos_segmentos, 0, 0), "constraint_axis":(True, False, False), "constraint_orientation":'GLOBAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
+
+bpy.ops.object.vertex_group_add()
+#bpy.context.object.name = "pin"    
+bpy.ops.object.vertex_group_assign()
+
+bpy.ops.mesh.select_all(action='TOGGLE')
+bpy.context.object.name = "jo"
+bpy.ops.mesh.select_all(action='TOGGLE')
+bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":(0, 0.005, 0), "constraint_axis":(False, True, False), "constraint_orientation":'GLOBAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
+
+
+
+bpy.ops.mesh.select_all(action='TOGGLE')
+bpy.ops.object.editmode_toggle()
+
+bpy.ops.object.modifier_add(type='CLOTH')
+bpy.context.object.modifiers["Cloth"].settings.use_pin_cloth = True
+bpy.context.object.modifiers["Cloth"].settings.vertex_group_mass = "Group"
