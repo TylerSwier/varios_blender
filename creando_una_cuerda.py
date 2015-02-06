824292ad1d3cd8f75e3f3762f1704500e3bc9e3b
zebus3d <zebus3dream@gmail.com>
2015-02-06 11:02:56 +0100

    Update creando_una_cuerda.py
    actualizando metodos de seleccion/deseleccion en editmode para que salgan resaltados los cambios

--- creando_una_cuerda.py
+++ creando_una_cuerda.py
@@ -19,18 +19,25 @@ def select_all_in_edit_mode(ob):
     if ob.mode != 'EDIT':
         entrar_en_editmode()
     bpy.ops.mesh.select_all(action="DESELECT")
+    bpy.context.tool_settings.mesh_select_mode = (True , False , False)
+    salir_de_editmode()
     for v in ob.data.vertices:
         if not v.select:
             v.select = True
-    bpy.ops.mesh.select_all(action="SELECT")        
+    entrar_en_editmode()
+    #bpy.ops.mesh.select_all(action="SELECT")        
 
 def deselect_all_in_edit_mode(ob):
     if ob.mode != 'EDIT':
         entrar_en_editmode()
+    bpy.ops.mesh.select_all(action="DESELECT")
+    bpy.context.tool_settings.mesh_select_mode = (True , False , False)
+    salir_de_editmode()
     for v in ob.data.vertices:
         if not v.select:
             v.select = False
-    bpy.ops.mesh.select_all(action="DESELECT")
+    entrar_en_editmode()
+    #bpy.ops.mesh.select_all(action="DESELECT")
 
 def which_vertex_are_selected(ob):
     for v in ob.data.vertices:
@@ -132,8 +139,21 @@ deselect_all_in_edit_mode(ob)
 
 salir_de_editmode()
 
-## DUPLICAMOS para convertir a curva:
-## duplicamos los vertices y los sacamos del object:
+# DUPLICAMOS para convertir a curva:
+# selecciono los vertices que forman parte del grupo Group.001
+seleccionar_por_nombre("cuerda")
+entrar_en_editmode()
+bpy.ops.mesh.select_all(action="DESELECT")
+bpy.context.tool_settings.mesh_select_mode = (True , False , False)
+salir_de_editmode()
+gi = ob.vertex_groups["Group.001"].index # get group index
+for v in ob.data.vertices:
+  for g in v.groups:
+    if g.group == gi: # compare with index in VertexGroupElement
+      v.select = True
+      
+entrar_en_editmode()
+      
 #bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1}, TRANSFORM_OT_translate={"value":(0, 0, 0), "constraint_axis":(False, False, False), "constraint_orientation":'GLOBAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
 ## separamos por seleccion
 #bpy.ops.mesh.separate(type'SELECTED')
@@ -144,7 +164,7 @@ bpy.context.object.modifiers["Cloth"].settings.vertex_group_mass = "Group"
 bpy.context.object.modifiers["Cloth"].collision_settings.collision_quality = calidad_de_colision
 bpy.context.object.modifiers["Cloth"].settings.quality = substeps
 
-deseleccionar_todo()
+#deseleccionar_todo()
 #seleccionar_por_nombre("cuerda.001")
 # convierto a curva:
 #bpy.ops.object.convert(target='CURVE')
