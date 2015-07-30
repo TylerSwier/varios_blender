25a931ac4b84012a97315f28041634f548ac7111
zebus3d <zebus3dream@gmail.com>
2015-07-30 11:07:22 +0200

    Update crear_cubitos_en_vertex_sphere_v00.py
    creando luces apuntando al target jeje

--- crear_cubitos_en_vertex_sphere_v00.py
+++ crear_cubitos_en_vertex_sphere_v00.py
@@ -1,5 +1,10 @@
 import bpy  
+
 ob = bpy.context.selected_objects[0]
+
+bpy.ops.object.empty_add(type='PLAIN_AXES', radius=1, view_align=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+bpy.context.object.name = "target_lights"
+
 vectores=[]
 for vertex in ob.data.vertices:
     vco = vertex.co
@@ -13,5 +18,10 @@ for c in vectores:
     y = c[1]
     z = c[2]
     if z >= 0:    
-        bpy.ops.mesh.primitive_cube_add(view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-        bpy.ops.transform.resize(value=(0.023373, 0.023373, 0.023373), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+        bpy.ops.object.lamp_add(type='SPOT', radius=1, view_align=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+        bpy.ops.object.constraint_add(type='TRACK_TO')
+        bpy.context.object.constraints["Track To"].target = bpy.data.objects["target_lights"]
+        bpy.context.object.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
+        bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
+        #bpy.ops.mesh.primitive_cube_add(view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+        #bpy.ops.transform.resize(value=(0.023373, 0.023373, 0.023373), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
