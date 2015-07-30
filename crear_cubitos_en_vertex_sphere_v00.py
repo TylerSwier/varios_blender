c5ea41dd5d67276e960b516f513834d43df5c1a6
zebus3d <zebus3dream@gmail.com>
2015-07-30 11:01:19 +0200

    Update crear_cubitos_en_vertex_sphere_v00.py
    solo agrega cubitos si es positivo en z ósea para hacer la hemisphere :D

--- crear_cubitos_en_vertex_sphere_v00.py
+++ crear_cubitos_en_vertex_sphere_v00.py
@@ -11,6 +11,7 @@ for vertex in ob.data.vertices:
 for c in vectores:
     x = c[0]
     y = c[1]
-    z = c[2]    
-    bpy.ops.mesh.primitive_cube_add(view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-    bpy.ops.transform.resize(value=(0.023373, 0.023373, 0.023373), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+    z = c[2]
+    if z >= 0:    
+        bpy.ops.mesh.primitive_cube_add(view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+        bpy.ops.transform.resize(value=(0.023373, 0.023373, 0.023373), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
