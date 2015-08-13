d40cd81f3be534abb9877abd0d7164e813ad512b
zebus3d <zebus3dream@gmail.com>
2015-08-13 17:43:20 +0200

    Update basics.py

--- basics.py
+++ basics.py
@@ -7,7 +7,7 @@ import bmesh
 # from zblibs.basics import *
 
 '''
-Copyright (c) 2012 Jorge Hernandez - Melendez
+2015 Jorge Hernandez - Melendez
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
