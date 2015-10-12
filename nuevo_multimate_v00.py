7b11c4cd5b3917dceac18b4bc123fbffcf6e9738
zebus3d <zebus3dream@gmail.com>
2015-10-12 18:33:15 +0200

    Update nuevo_multimate_v00.py

--- nuevo_multimate_v00.py
+++ nuevo_multimate_v00.py
@@ -1,29 +1,49 @@
 import bpy
 
-def createNewScene(name, retornamos=True, motor='CYCLES'):
+def selectAll():
+    bpy.ops.object.select_all(action='SELECT')
+    
+def deselectAll():
+    bpy.ops.object.select_all(action='DESELECT')
+
+def selectOnlyOneObjectByName(obn,scn = bpy.context.scene):
+    ob = bpy.context.scene.objects[obn]
+    bpy.ops.object.select_all(action='DESELECT')
+    scn.objects.active = ob
+    ob.select = True
+
+def createNewScene(name, tipo='NEW', retornamos=False, motor='CYCLES'):
     scn_original = bpy.context.scene.name
-    bpy.ops.scene.new(type='NEW')
+    if tipo == 'NEW':
+        bpy.ops.scene.new(type='NEW')
+    if tipo == 'FULL_COPY':
+        bpy.ops.scene.new(type='FULL_COPY')
     bpy.context.scene.name = name
-    if retornamos:
-        new_scn = bpy.context.scene.name
-        bpy.context.screen.scene = bpy.data.scenes[scn_original]
-        return new_scn
-    else:
-        # si no retornamos devolvemos el nombre de la nueva escena
-        return bpy.context.scene.name
-    # si no se especifica motor se usara cycles en la nueva escena
-    if motor:
+    new_scn = bpy.context.scene.name
+    if motor == 'CYCLES':
         bpy.context.scene.render.engine = motor
+    if retornamos:
+        bpy.context.screen.scene = bpy.data.scenes[scn_original]
+    return [scn_original,new_scn]
 
-# creamos una escena nueva
-name_scn = createNewScene("RGB_Pass")
-#print(name_scn)
+# para hacer los materiales unicos y no vinculados (Make a single user copy):
+def aplicar_shader_copy(ob,nombre):
+    ob.active_material = bpy.data.materials[nombre].copy()
 
-# seleccionamos todos los mesh y camaras
-bpy.ops.object.select_all(action='DESELECT')
-for ob in bpy.data.objects:
-    if ob.type == 'MESH' or ob.type == 'CAMERA':
-        ob.select = True
-        
-# enviamos los objetos seleccionados a la ultima nueva escena:
-bpy.ops.object.make_links_scene(scene=name_scn)
+if 'Backup_Original_Scene' not in bpy.data.scenes:
+    createNewScene('Backup_Original_Scene','FULL_COPY', True)
+
+createNewScene('test','NEW',True)
+for ob in bpy.context.scene.objects:
+    if ob.type == 'MESH': 
+        deselectAll()
+        selectOnlyOneObjectByName(ob.name)
+        name_org = ob.name
+        bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
+        bpy.context.selected_objects[0].name = bpy.context.selected_objects[0].name + "_copy" 
+        bpy.ops.object.make_links_scene(scene='test')
+        selectOnlyOneObjectByName(bpy.context.selected_objects[0].name)
+        bpy.ops.object.delete(use_global=False)
+        selectOnlyOneObjectByName(ob.name)
+        mat = bpy.context.selected_objects[0].material_slots[0].name
+        aplicar_shader_copy(ob, mat)
