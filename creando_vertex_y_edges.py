ff9c75c2076154c32ed7d1fe023397e9aa97fc1f
zebus3d <zebus3dream@gmail.com>
2016-06-19 16:01:03 +0200

    Update creando_vertex_y_edges.py
    creando caras

--- creando_vertex_y_edges.py
+++ creando_vertex_y_edges.py
@@ -14,9 +14,11 @@ def createMeshes(name, vertex=[], edges=[], faces=[]):
     bpy.context.scene.objects.link(ob)
     return ob
 
-# las coordenadas de cada vertice:
-vertices = [[0,0,0],[1,1,1],[2,2,2]]
+# las coordenadas de cada vertice:  
+vertices = [[-1,0,0],[-1,1,0],[0,1,-1],[0,0,0]]
 # de que vertice a que vertice hace el edge:
-aristas = [[0,1],[1,2]] # el id de cara vertice
-ob = createMeshes('test', vertices, aristas )
+aristas = [[0,1],[1,2],[2,3],[3,4]] # el id de cara vertice
+caras = [[0,1,2,3]] # el id de los vertices para esa cara
+# para hacer caras no hay que especificar tambien las aristas:
+ob = createMeshes('test', vertices, [], caras)
 #print(ob.name)
