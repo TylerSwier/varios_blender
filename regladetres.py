daf98a7c81856ba711c0ee0c99189b027e04170e
zebus3d <zebus3dream@gmail.com>
2016-10-01 11:32:10 +0200

    Update regladetres.py

--- regladetres.py
+++ regladetres.py
@@ -34,7 +34,7 @@ from bpy.props import *
 bpy.types.Scene.etxt1 = StringProperty(name="", description="Si A", maxlen= 1024, default="Si A (cantidad)") 
 bpy.types.Scene.etxt2 = StringProperty(name="", description="Son B", maxlen= 1024, default="Son B (cantidad)") 
 bpy.types.Scene.etxt3 = StringProperty(name="", description="Entonces C son", maxlen= 1024, default="Cuanto sera C?")
-bpy.types.Scene.etxt4 = StringProperty(name="", attr="res", description="resultado", maxlen= 1024, default="C son...") 
+bpy.types.Scene.etxt4 = StringProperty(name="", attr="res", description="resultado", maxlen= 1024, default="") 
 
 class reglaDeTres(bpy.types.Panel): 
     bl_label = "Regla de tres simple v00:" 
@@ -51,18 +51,24 @@ class reglaDeTres(bpy.types.Panel):
         col1.alignment = 'EXPAND' 
 
         col1.prop(context.scene,"etxt1") 
-        col1.prop(context.scene,"etxt2") 
-        
-        col1.prop(context.scene,"etxt3")         
-        
+        col1 = row1.column() 
+        col1.prop(context.scene,"etxt2")  
+        row2 = layout.row(align=True) 
+        col2 = row2.column()
+        col1 = row1.column() 
         col1.operator("calcular.calcular", text='Calcular')
-        col1.prop(context.scene,"etxt4") 
+        col2.prop(context.scene,"etxt3")
+        col2.label(text="Resultado: " + bpy.context.scene.etxt4)
+        
+        
+        
         col1 = split.column() 
-         
+
          
 def calcular(): 
     resultado = str( float(bpy.context.scene.etxt3) * float(bpy.context.scene.etxt2) / float(bpy.context.scene.etxt1) ) 
     return resultado 
+
 class botonAccion(bpy.types.Operator): 
     bl_idname = "calcular.calcular" 
     bl_label = "calcular" 
@@ -71,7 +77,7 @@ class botonAccion(bpy.types.Operator):
         try: 
             bpy.context.scene.etxt4 = calcular() 
         except: 
-            bpy.context.scene.etxt4 = "Solo son validos valores numericos!!" 
+            bpy.context.scene.etxt3 = "Solo son validos valores numericos!!" 
         return{'FINISHED'} 
 
 def register(): 
