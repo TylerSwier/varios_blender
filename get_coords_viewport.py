dba3b632d6e3e95dbfaea4da76a8fc11eb7a5e8f
zebus3d <zebus3dream@gmail.com>
2017-02-23 14:33:08 +0100

    Create get_coords_viewport.py

---
+++ get_coords_viewport.py
@@ -0,0 +1,31 @@
+import bpy
+
+def convert_matrix_to_location(matrix):
+    """ From 4x4 matrix, calculate camera location """
+    t = (matrix[0][3], matrix[1][3], matrix[2][3])
+    r = (
+      (matrix[0][0], matrix[0][1], matrix[0][2]),
+      (matrix[1][0], matrix[1][1], matrix[1][2]),
+      (matrix[2][0], matrix[2][1], matrix[2][2])
+    )
+    rp = (
+      (-r[0][0], -r[1][0], -r[2][0]),
+      (-r[0][1], -r[1][1], -r[2][1]),
+      (-r[0][2], -r[1][2], -r[2][2])
+    )
+    output = (
+      rp[0][0] * t[0] + rp[0][1] * t[1] + rp[0][2] * t[2],
+      rp[1][0] * t[0] + rp[1][1] * t[1] + rp[1][2] * t[2],
+      rp[2][0] * t[0] + rp[2][1] * t[1] + rp[2][2] * t[2],
+    )
+    return output
+
+# Get coords from viewport (this return matrix 4x4):
+for area in bpy.context.screen.areas: # iterate through areas in current screen
+    if area.type == 'VIEW_3D':
+        for space in area.spaces: # iterate through spaces in current VIEW_3D area
+            if space.type == 'VIEW_3D': # check if space is a 3D view
+                loc = convert_matrix_to_location(space.region_3d.view_matrix)
+                
+# test create cube in viewport position:
+bpy.ops.mesh.primitive_cube_add(view_align=False, enter_editmode=False, location=(loc), layers=(False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
