7da1b16a56c3c5313b9028c95ba4fbe027bd1473
zebus3d <zebus3dream@gmail.com>
2017-04-24 00:02:02 +0200

    Update myChamfer.py
    contribuciones

--- myChamfer.py
+++ myChamfer.py
@@ -5,6 +5,7 @@ from mathutils import Vector
 
 '''
 Copyright (c) 2012 Jorge Hernandez - Melendez
+Contributors: Carlos Ruiz Garin
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
