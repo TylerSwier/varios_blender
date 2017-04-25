de7f237676727b7cb0f0984cc6d8a15ee6d24ace
zebus3d <zebus3dream@gmail.com>
2017-04-25 21:22:46 +0200

    Update explode_de_objetos.py
    hallar el centroid de una forma mejor.

--- explode_de_objetos.py
+++ explode_de_objetos.py
@@ -3,13 +3,34 @@ import math
 from mathutils import Vector
 
 # la manera mas sencilla que he encontrado de obtener el centroid de una figura irregular:
+#def getCentroid():
+#    for area in bpy.context.screen.areas:
+#        if area.type == 'VIEW_3D':
+#            ctx = bpy.context.copy()
+#            ctx['area'] = area
+#            bpy.ops.view3d.snap_cursor_to_selected(ctx)
+#            return bpy.context.scene.cursor_location[:]
+
+# nueva forma de hallar el centroid:
 def getCentroid():
-    for area in bpy.context.screen.areas:
-        if area.type == 'VIEW_3D':
-            ctx = bpy.context.copy()
-            ctx['area'] = area
-            bpy.ops.view3d.snap_cursor_to_selected(ctx)
-            return bpy.context.scene.cursor_location[:]
+    # hallar el centroid de la seleccion:
+    obx = []  # <- aqui iremos guardando las coordenadas x de todos los objetos
+    oby = []  # <- aqui iremos guardando las coordenadas y de todos los objetos
+    obz = []  # <- aqui iremos guardando las coordenadas z de todos los objetos
+
+    for ob in bpy.context.selected_objects:
+        obx.append(ob.location.x)
+        oby.append(ob.location.y)
+        obz.append(ob.location.z)
+
+    # el total de objetos:
+    totalObjects = len(bpy.context.selected_objects)
+
+    # obtenemos el centroid:
+    # sum() realiza internamente la suma de los elementos de una lista
+    # y lo dividimos por el numero total de elementos :D
+    centroid = (sum(obx) / totalObjects, sum(oby) / totalObjects, sum(obz) / totalObjects)
+    return centroid
 
 # para guardar las coordenadas originales de cada objeto:
 originalCoords = []
